34675406e60e8a05a2a897bc838ca29f4b3a6d8d
Jeremy Varghese <jeremy.varghese11@gmail.com>
2023-11-11 18:35:33 -0500

    Mayor votes works. Backend integrated

--- src/Components/VoteMayor.js
+++ src/Components/VoteMayor.js
@@ -3,71 +3,46 @@ import 'bootstrap/dist/css/bootstrap.css';
 import axios, { all } from "axios";
 import NavBar from "./NavBar";
 import Footer from "./footer";
+import Popup from "reactjs-popup";
 
 function VoteMayor(props) {
   const [candidates, setCandidates] = useState([]);
-  const [selectedCandidate, setSelectedCandidate] = useState(null);
-  const [selectedIndex, setSelectedIndex] = useState(null);
-  const [voted, setVoted] = useState(null);
-
+  const [selectedCandidate, setSelectedCandidate] = useState("");
+  const [voted, setVoted] = useState("No");
   const [userID, setUserID] = useState([]);
   const [yesCount, setYesCount] = useState([]);
-  const [users, setUsers] = useState([]);
 
   const user_id = localStorage.getItem("user_id");
-  //const allNames = [];
   useEffect(() => {
     axios.get("http://localhost:9000/getCandidates")
       .then((res) => {
         if (res.data) {
           setCandidates(res.data);
-          //console.log(res.data);
-          const candidateUsers = [];
-          for (const cand of res.data) {
-            axios.get("http://localhost:9000/getUserById", {
-              params: {
-                _id: cand.userID,
-              }
-            })
-              .then((res) => {
-                if (res.data) {
-                  //console.log(res.data.firstName);
-                  cand.firstName = res.data.firstName;
-                  candidateUsers.push(res.data);
-                }
-                else {
-                  alert("Name not found");
-                }
-              })
-          }
-          //console.log(candidateNames);
-          setUsers(candidateUsers);
         }
       }).catch((err) => {
         console.log(`SERVER ERROR: ${err}`);
-        //alert(`ERROR: ${err}`)
       })
-    /*axios.get('http://localhost:9000/getMayorVotes')
+    axios.get('http://localhost:9000/getMayorVotes')
       .then((res) => {
-        if(res.data){
-        setCandidates(res.data.candidates);
-        setUserID(res.data.userID);
-        setYesCount(res.data.yesCount);
+        if (res.data) {
+
+          setUserID(res.data[0].userID);
+          setYesCount(res.data[0].yesCount);
+          if (res.data[0].userID.includes(user_id)) {
+            setVoted("Yes");
+            setSelectedCandidate()
+          }
         }
-        else
-        {
+        else {
           alert("Fetching mayorVotes table failed");
         }
       })
-      .catch((err) => alert('Error in Fetching Candidates'))
-    */
-    //setCandidates([{ "name": 'A', "id": 1 }, { "name": 'B', "id": 2 }, { "name": 'C', "id": 3 }, { "name": 'D', "id": 4 }]);
+      .catch((err) => {
+        console.log(`SERVER ERROR: ${err}`);
+      })
+
   }, []);
-  //console.log(names);
-  const getCandidateName = (id) => {
-    const candidate = candidates.find((candidate) => candidate._id == id);
-    return candidate ? candidate.firstName : 'Candidate Name Not Found';
-  }
+
 
   const handleRadioChange = (event) => {
     setSelectedCandidate(event.target.value);
@@ -77,18 +52,33 @@ function VoteMayor(props) {
     if (selectedCandidate == null) {
       alert('No Candidate Selected');
     } else {
-      alert(`You voted for Mayor : ${getCandidateName(selectedCandidate)}`);
+      const cand = candidates.find((candidate) => candidate._id == selectedCandidate);
+      alert(`You voted for Mayor : ${cand.firstName} ${cand.lastName}`);
       setVoted({ "id": user_id, "vote": selectedCandidate })
-      /*let votes = [...yesCount];
-      let vote = votes[selectedCandidate];
-      vote += 1;
-      votes[selectedCandidate] = vote;
-      setYesCount(votes);
-      axios.post('https://localhost:9000/voteMayor', {
-        "userID": [...userId, user_id],
-        "candidateID": candidates,
-        "yesCount": yesCount,
-      })*/
+      const votes = yesCount.find((vote) => vote.candidateId == cand._id);
+      let vote = votes.votes;
+      vote = vote + 1;
+      var index = yesCount.indexOf(votes);
+      const updatedVote = new Object({
+        candidateId: votes.candidateId,
+        votes: vote,
+      })
+      const yesses = [...yesCount]
+      yesses[index] = updatedVote;
+      const users = userID;
+      users.push(user_id);
+      console.log(yesses);
+      axios.post("http://localhost:9000/postMayorVotes", { userID: users, candidateID: candidates, yesCount: yesses })
+        .then((res) => {
+          //something
+          //console.log(res.data)
+          alert(`Vote Submitted Successfully!`)
+
+        })
+        .catch((err) => {
+          console.log(`SERVER ERROR: ${err}`);
+          alert(`ERROR: ${err}`)
+        })
     }
   };
 
@@ -98,21 +88,30 @@ function VoteMayor(props) {
       <div className="container">
         <div className="row justify-content-center align-items-center lcontainer team-list">
           <h4 style={{ color: 'green' }}>Vote Mayor</h4> <br /> <br />
-          {voted != null ? (
-            <h4>You have already voted for {getCandidateName(voted.vote)}</h4>
+          {voted != "No" ? (
+            <h4>You have already voted</h4>
           ) : (
             <>
               <table className="table table-striped table-bordered table-hover">
                 <thead>
                   <tr>
                     <th>Name</th>
+                    <th>Policies</th>
                     <th>Vote</th>
                   </tr>
                 </thead>
                 <tbody>
-                  {candidates.length > 0 && candidates.map((candidate) => (
-                    <tr key={candidate.id}>
-                      <td>{candidate.firstName}</td>
+                  {candidates.map((candidate) => (
+                    <tr key={candidate._id}>
+                      <td>{candidate.firstName} {candidate.lastName}</td>
+                      <td><Popup trigger={<button> Policies</button>} position="right center" modal nested>
+                        {
+                          candidate.policies.map((policy) => (
+                            <div>{policy}</div>
+                          ))
+                        }
+
+                      </Popup></td>
                       <td>
                         <input
                           type="radio"
@@ -125,9 +124,7 @@ function VoteMayor(props) {
                   ))}
                 </tbody>
               </table>
-              {selectedCandidate != null && (
-                <h5>You have selected to vote for: {getCandidateName(selectedCandidate)}</h5>
-              )}
+
               <button type="button" onClick={onClickHandler} className="btn btn-outline-primary">
                 Vote
               </button>
@@ -139,5 +136,4 @@ function VoteMayor(props) {
     </div>
   );
 }
-
 export default VoteMayor;
