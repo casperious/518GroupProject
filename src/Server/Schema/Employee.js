fae7029854a53bb4e4a3ccc7baff6bf9ff36f17f
Jeremy Varghese <jeremy.varghese11@gmail.com>
2023-11-13 09:37:53 -0500

    Employee and contract schema

--- src/Server/Schema/Employee.js
+++ src/Server/Schema/Employee.js
@@ -2,9 +2,9 @@ const mongoose = require('../utils/mongoDB');
 
 const EmployeeSchema = new mongoose.Schema({
     dept_id: mongoose.Schema.Types.ObjectId,
-    emp_id: mongoose.Schema.Types.ObjectId,
+    user_id: mongoose.Schema.Types.ObjectId,
     jobDescription: String,
-    
+    contract_id: mongoose.Schema.Types.ObjectId,
 });
 
 const Employee = mongoose.model("Employee", EmployeeSchema);
